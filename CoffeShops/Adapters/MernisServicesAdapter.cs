9dbe0bd594112aeaee7fc0c53bbafaafe14b298d
Birol-isgoren <birolisgoren@gmail.com>
2021-03-19 23:59:04 +0200

    First Checkin

---
+++ CoffeShops/Adapters/MernisServicesAdapter.cs
@@ -0,0 +1,19 @@
+﻿using CoffeShops.Abstract;
+using CoffeShops.MernisServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShops.Adapters
+{
+    public class MernisServicesAdapter:ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Entities.Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(long.Parse(customer.NationaltyId), customer.FirstName.ToUpper(), customer.LastName.ToUpper(), customer.DateOfBirth.Year);
+        }
+    }
+}
---
+++ CoffeShops/Program.cs
@@ -0,0 +1,24 @@
+﻿using CoffeShops.Abstract;
+using CoffeShops.Adapters;
+using CoffeShops.Concrete;
+using CoffeShops.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeShops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServicesAdapter());
+            customerManager.Save(new Customer { FirstName= "Birol", LastName ="işgören", NationaltyId = "12345678901", DateOfBirth = new DateTime(1902,11,13)});
+            BaseCustomerManager cust = new NeroCustomerManager();
+            Console.ReadLine();
+        }
+
+    }
+}
